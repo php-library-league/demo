5f9241364393e1a4c5907667583516485aa74e35
90zlaya <contact@zlatanstajic.com>
2018-04-11 08:52:54 +0200

    demonstration scripts update for PHP Library v1.5.0

--- Operating_System.php
+++ Operating_System.php
@@ -13,6 +13,4 @@ include '../vendor/autoload.php';
 use phplibrary\Operating_System as operating_system;
 use phplibrary\Format as format;
 
-format::pre(
-    operating_system::get_list()
-);
+format::pre(operating_system::get_list());
--- Sorter.php
+++ Sorter.php
@@ -13,15 +13,24 @@ include '../vendor/autoload.php';
 use phplibrary\Sorter as sorter;
 use phplibrary\Format as format;
 
+$destination  = realpath('../outsource/sorter/');
+$destination .= DIRECTORY_SEPARATOR;
+$destination .= 'destination';
+
+if ( ! is_dir($destination))
+{
+    mkdir($destination);
+}
+
 $sorter = new sorter();
 $report = $sorter->deploy(array(
-    'where_to_read_files'         => 'D:/Zlatan/Browser/sorter/source/',
-    'where_to_create_directories' => 'D:/Zlatan/Browser/sorter/destination/',
+    'where_to_read_files'         => realpath('../outsource/sorter/source/') . DIRECTORY_SEPARATOR,
+    'where_to_create_directories' => realpath('../outsource/sorter/destination/') . DIRECTORY_SEPARATOR,
     'number_of_directories'       => 10,
     'folder_sufix'                => '000',
     'operation'                   => 'c',
     'types'                       => array('jpg'),
 ));
 
-echo $report['string'];
+format::pre($report['string']);
 format::pre($report['array'], TRUE);
