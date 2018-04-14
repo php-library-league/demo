8c25c94d851465458b09dca9279d5234f8b235cd
90zlaya <contact@zlatanstajic.com>
2018-04-14 10:49:17 +0200

    validation.php update

--- Validation.php
+++ Validation.php
@@ -13,15 +13,14 @@ include '../vendor/autoload.php';
 use phplibrary\Validation as validation;
 use phplibrary\Format as format;
 
-$name             = '90zlaya.jpeg';
-$valid_extensions = array("jpeg");
-$valid_types      = array("image/jpeg");
-
-echo validation::extension($name, $valid_extensions, 'image/jpeg', $valid_types);
-echo '<br/>';
+format::pre(validation::extension(
+    '90zlaya.jpeg',
+    array('jpeg'),
+    'image/jpeg',
+    array('image/jpeg')
+));
 
 $string = 'Ovo je test NAZIV 12  razmak 34 ima i đĐčćŠ.png';
 
-echo validation::rewrite($string);
-echo '<br/>';
-echo validation::rewrite_special($string);
+format::pre(validation::rewrite($string));
+format::pre(validation::rewrite_special($string));
